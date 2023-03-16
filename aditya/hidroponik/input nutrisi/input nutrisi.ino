157a5860d3d4729459bc61c920a358dc1b46a754
adityh12 <aditynh12@gmail.com>
2023-03-16 11:58:01 -0700

    save

--- aditya/hidroponik/input nutrisi/input nutrisi.ino
+++ aditya/hidroponik/input nutrisi/input nutrisi.ino
@@ -4,6 +4,7 @@
 #include <Keypad.h>
 #include <EEPROM.h>
 #include <DS3231.h>
+#include <LinearRegression.h>
 #include "GravityTDS.h"
 #define TdsSensorPin A1
 #define DS3231_I2C_ADDRESS 104
@@ -11,7 +12,9 @@ float nilai[6];
 float nilaiTDS[6];
 float mTDS[7];
 float bTDS[7];
-float tdsValue = 0;
+GravityTDS gravityTds;
+float tdsValue = 0, value = 0;
+double nilaiKalibrasiTDS[2] = { 0, 0 };
 int relay = 2;    //nutrisi A
 int relay2 = 3;   //nutrisi B
 int relay3 = 12;  //pompa air
@@ -21,7 +24,6 @@ const byte BARIS = 4;
 const byte KOLOM = 4;
 char buff[16];
 DS3231 rtc(SDA, SCL);  //real time clock connect
-GravityTDS gravityTds;
 byte seconds, minutes, hours, day, date, month, year;
 char tombol[BARIS][KOLOM] = {
   { '1', '2', '3', 'A' },
@@ -52,66 +54,43 @@ int menu = 0;
 int secupg;
 bool blinking = true;
 
-Time t;
+Time time;
+Time currentTime;
+LinearRegression lr;
 
-void setup() {
-  //lcd.begin(4, 20);
-  // lcd.backlight();
-  // lcd.setCursor(0, 0);
-  //lcd.print("input nutrisi:");
-  lcd.begin(4, 20);
-  lcd.backlight();
-  gravityTds.setPin(TdsSensorPin);
-  gravityTds.setAref(5.0);       //reference voltage on ADC, default 5.0V on Arduino UNO
-  gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
-  gravityTds.begin();            //initialization
-  pinMode(TdsSensorPin, INPUT);
-}
-
-void loop() {
-  //float tds = analogRead();
-  lcd.setCursor(0, 0);
-  lcd.print("1. jalankan alat");
-  lcd.setCursor(0, 1);
-  lcd.print("2. kalibrasi TDS");
-  lcd.setCursor(0, 2);
-  lcd.print("3. dosis");
-  char menuTerpilih = getCharFromKeypad();
-  switch (menuTerpilih) {
-    case '1':
-      jalankanAlat();
-      break;
-    case '2':
-      kalibrasi();
-      break;
-    case '3':
-      pengaturanDosis();
-      break;
-  }
-}
 bool jalankanAlat() {
   long timeAcuan;
-  int mgke;
+  float bTDS;
+  float mTDS;
+  EEPROM.get(0, mTDS);
+  EEPROM.get(5, bTDS);
   EEPROM.get(50, timeAcuan);
-  //TODO: get current time dari RTC
-  {
-    // Print a message to the LCD.
-    lcd.setCursor(3, 0);
-    lcd.print(rtc.getDateStr());  // Print date to LCD
-    lcd.setCursor(0, 1);
-    lcd.print(rtc.getTimeStr());  // Print time to LCD
-    lcd.setCursor(0, 2);
-
-    lcd.print(rtc.getDOWStr());
-  }
+  rtc.begin();  //begin real time clock
+  lcd.setCursor(3, 0);
+  lcd.print(rtc.getDateStr());  // Print date to LCD
+  lcd.setCursor(0, 1);
+  lcd.print(rtc.getTimeStr());  // Print time to LCD
+  lcd.setCursor(0, 2);
+  lcd.print(rtc.getDOWStr());
   float dosis;
-  int address = mgke * 5 + 20;
-  EEPROM.get(address, dosis);
-  
+
   while (1) {
-    louy lama = cureenTime - timeAwal;
-    int mg ke = 1;
-    if(lama < 168 jam);{
+    currentTime = rtc.getTime();
+    long timeNumber = rtc.getUnixTime(currentTime);
+
+    long lama = timeNumber - timeAcuan;
+    tdsValue = gravityTds.getTdsValue();
+    float nilaiTDS = mTDS * tdsValue + bTDS;
+    if (lama < 86400000) {
+      EEPROM.get(20, dosis);      
+    }else if (lama > 86400000 && lama < 1209600000) {
+      EEPROM.get(25, dosis);      
+    }else if (lama > 1209600000 && lama < 1814400000) {
+      EEPROM.get(30, dosis);      
+    }else if (lama > 1814400000 && lama < 2419200000) {
+      EEPROM.get(35, dosis);      
+    }      
+    if (nilaiTDS < dosis) {
       delay(3000);
       digitalWrite(relay3, HIGH);
       digitalWrite(relay, LOW);
@@ -119,38 +98,41 @@ bool jalankanAlat() {
       delay(300);
       digitalWrite(relay4, LOW);
       delay(5000);
-    } else if (lama > 1176 jam) {
+    } else {
       delay(3000);
       digitalWrite(relay3, LOW);
       digitalWrite(relay, HIGH);
       digitalWrite(relay2, HIGH);
+      delay(300);
       digitalWrite(relay4, HIGH);
-    }
-  }
+      delay(5000);
+    }    
+  } 
 }
-bool kalibrasi() {
 
-  lcd.clear();
+bool kalibrasi() {
   lcd.clear();
   for (int i = 0; i < 6; i++) {
     lcd.setCursor(0, 0);
     lcd.print("nilai : ");
     lcd.print(i + 1);
     float value = getFloatFromKeypad(buff);
-    int j = i * 5 + 0;
-    EEPROM.put(j, value);
+    tdsValue = gravityTds.getTdsValue();  // then get the value
+    lr.learn(value, tdsValue);
 
-    lcd.setCursor(0, 1);
-    lcd.print("TDS Value:");
-    lcd.print(tdsValue, 0);
-    EEPROM.put(j, tdsValue);
     delay(500);
   }
+
+  lr.getValues(nilaiKalibrasiTDS);
+  float mTDS = (float)nilaiKalibrasiTDS[0];
+  float bTDS = (float)nilaiKalibrasiTDS[1];
+  EEPROM.put(0, mTDS);
+  EEPROM.put(5, bTDS);
 }
 bool pengaturanDosis() {
   rtc.begin();  //begin real time clock
-  t = rtc.getTime();
-  long currentTime = rtc.getUnixTime(t);
+  time = rtc.getTime();
+  long currentTime = rtc.getUnixTime(time);
 
   lcd.clear();
   for (int i = 0; i < 6; i++) {
@@ -188,5 +170,41 @@ float getFloatFromKeypad(char* arr) {
         cursorColom++;
       }
     }
+  }
+}
+void setup() {
+  //lcd.begin(4, 20);
+  // lcd.backlight();
+  // lcd.setCursor(0, 0);
+  //lcd.print("input nutrisi:");
+  lcd.begin(4, 20);
+  lcd.backlight();
+  gravityTds.setPin(TdsSensorPin);
+  gravityTds.setAref(5.0);       //reference voltage on ADC, default 5.0V on Arduino UNO
+  gravityTds.setAdcRange(1024);  //1024 for 10bit ADC;4096 for 12bit ADC
+  gravityTds.begin();            //initialization
+  pinMode(TdsSensorPin, INPUT);
+  lr = LinearRegression();
+}
+
+void loop() {
+  //float tds = analogRead();
+  lcd.setCursor(0, 0);
+  lcd.print("1. jalankan alat");
+  lcd.setCursor(0, 1);
+  lcd.print("2. kalibrasi TDS");
+  lcd.setCursor(0, 2);
+  lcd.print("3. dosis");
+  char menuTerpilih = getCharFromKeypad();
+  switch (menuTerpilih) {
+    case '1':
+      jalankanAlat();
+      break;
+    case '2':
+      kalibrasi();
+      break;
+    case '3':
+      pengaturanDosis();
+      break;
   }
 }
